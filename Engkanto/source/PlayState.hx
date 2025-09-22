7ccc7f62a404fcb50fa3fd957e22da24dc6debad
x40arcade <sobrepenakenneth97@gmail.com>
2025-09-22 21:24:55 +0800

    SpriteSheet Animation and Display
    Engkanto #1 Done
    https://github.com/users/SobrepenaKenneth/projects/3?pane=issue&itemId=130074956

--- Engkanto/source/PlayState.hx
+++ Engkanto/source/PlayState.hx
@@ -1,12 +1,20 @@
 package;
 
+import Intro;
 import flixel.FlxState;
+import haxe.iterators.StringIteratorUnicode;
 
 class PlayState extends FlxState
 {
+	var intro:Intro;
+
 	override public function create()
 	{
 		super.create();
+		intro = new Intro();
+
+		add(intro.butterfly);
+
 	}
 
 	override public function update(elapsed:Float)
