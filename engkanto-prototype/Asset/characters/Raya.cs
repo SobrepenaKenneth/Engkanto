2b3a52cee5e89524dbc453537ccd81389cb1dd58
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-15 16:51:37 +0800

    Added Cutscene_Scene

---
+++ engkanto-prototype/Asset/characters/Raya.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+using System.Numerics;
+
+public partial class Raya : CharacterBody2D
+{
+    public float Speed = 300;
+
+    public override void _PhysicsProcess(double delta)
+    {
+
+
+    }
+
+	
+}
