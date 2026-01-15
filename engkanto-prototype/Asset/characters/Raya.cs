505a05eda404cfc9951cc179ac0446c771da7c79
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-15 21:21:06 +0800

    Raya test placeholder

--- engkanto-prototype/Asset/characters/Raya.cs
+++ engkanto-prototype/Asset/characters/Raya.cs
@@ -1,16 +1,35 @@
 using Godot;
 using System;
-using System.Numerics;
 
 public partial class Raya : CharacterBody2D
 {
-    public float Speed = 300;
+	public string name = "Raya";
+	public float speed = 300;
+	public float gravity = 1200;
+	public float jump_Force = -600;
 
-    public override void _PhysicsProcess(double delta)
-    {
+	public override void _Ready()
+	{
+		
+	}
 
+	public override void _Process(double delta)
+	{
+		if (!IsOnFloor())
+		{
+			Velocity += new Vector2(0, gravity * (float)delta);
+		}
 
-    }
+		float direction = 0;
+		if (Input.IsActionPressed("ui_right")) direction += 1;
+		if (Input.IsActionPressed("ui_left")) direction -= 1;
+		
+		Velocity = new Vector2(direction * speed, Velocity.Y);
 
-	
+		if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+		{
+			Velocity = new Vector2(Velocity.X, jump_Force);
+		}
+		MoveAndSlide();
+	}
 }
