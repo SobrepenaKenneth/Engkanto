89a6b7a605db7254cfbe9b8d992bc471cda30b94
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-16 16:01:38 +0800

    Collision Test

--- engkanto-prototype/Asset/characters/Raya.cs
+++ engkanto-prototype/Asset/characters/Raya.cs
@@ -3,33 +3,41 @@ using System;
 
 public partial class Raya : CharacterBody2D
 {
-	public string name = "Raya";
-	public float speed = 300;
-	public float gravity = 1200;
-	public float jump_Force = -800;
+	// Using [Export] allows you to edit these values in the Inspector 
+	// without having to recompile the code.
+	[Export] public float Speed = 300.0f;
+	[Export] public float Gravity = 2000.0f;
+	[Export] public float JumpForce = -600.0f;
 
-	public override void _Ready()
+	public override void _PhysicsProcess(double delta)
 	{
-		
-	}
+		Vector2 velocity = Velocity;
 
-	public override void _Process(double delta)
-	{
+		// 1. Apply Gravity
 		if (!IsOnFloor())
 		{
-			Velocity += new Vector2(0, gravity * (float)delta);
+			velocity.Y += Gravity * (float)delta;
 		}
-
-		float direction = 0;
-		if (Input.IsActionPressed("ui_right")) direction += 1;
-		if (Input.IsActionPressed("ui_left")) direction -= 1;
-		
-		Velocity = new Vector2(direction * speed, Velocity.Y);
-
-		if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+		else
 		{
-			Velocity = new Vector2(Velocity.X, jump_Force);
+			// Reset vertical velocity when on floor to prevent physics bugs
+			velocity.Y = 0; 
 		}
+
+		// 2. Handle Jumping
+		// Make sure "jump" is defined in Project Settings -> Input Map
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpForce;
+		}
+
+		// 3. Handle Horizontal Movement
+		// GetAxis returns -1 for left, 1 for right, and 0 for nothing
+		float direction = Input.GetAxis("ui_left", "ui_right");
+		velocity.X = direction * Speed;
+
+		// 4. Apply the velocity and move
+		Velocity = velocity;
 		MoveAndSlide();
 	}
 }
